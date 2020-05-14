4d1536dc8296703419a03dc828625730de1f7513
aussendorf <maik.aussendorf@bareos.com>
2020-05-14 12:32:48 +0200

    python-split-job-plugin: added systemtest for parallel and serial incremental jobs
    - testrunner runs the 3 configured splitjobs in parallel
    - testrunner.serial runs the test one after another

---
+++ systemtests/tests/python-fd-plugin-split-jobs-test/gen_test_files.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+import string
+import tempfile
+import sys
+import os
+import random
+import time
+
+def randstring(l):
+    return ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(l))
+
+def mkdir(directory_name):
+    """
+    Utility Function for creating directories
+    """
+    try:
+        os.stat(directory_name)
+    except OSError:
+        os.makedirs(directory_name)
+
+l1 = 10 
+l2 = 10
+l3 = 10
+l4 = 100
+
+print "Generating %d * %d * %d * %d = %d Testfiles ..." % (l1, l2, l3, l4, l1*l2*l3*l4)
+
+for i in range(1, l1+1):
+    di = "%d-%s" % (i, randstring(15))
+    for j in range(1, l2+1):
+        dj = "%d-%s" % (j, randstring(random.randint(20,30)))
+        for k in range(1, l3+1):
+          print "i=%d j=%d, k=%d" % (i, j, k)
+          dk = "%d-%s" % (k, randstring(random.randint(30,40)))
+          dir_name = "%s/%s/%s" % (di, dj, dk)
+          mkdir(dir_name)
+          for l in range(1,l4+1):
+              file_name = randstring(random.randint(10,70))
+              f = tempfile.NamedTemporaryFile(dir=dir_name, prefix=file_name, delete=False)
+              f.write(time.strftime("%d %b %Y %H:%M:%S") + "\n")
+              f.close()
